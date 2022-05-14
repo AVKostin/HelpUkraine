e1bde7e8683f5b4ce9861516460b33f1f0e1a172
AVKostin <90350582+AVKostin@users.noreply.github.com>
2022-05-14 00:33:25 +0000

    Deploying to gh-pages from @ AVKostin/HelpUkraine@30f2170e71c43e26ba025dc5d90be00e9111eda2 🚀

--- src.7d606b04.js
+++ src.7d606b04.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../fonts/Retroica.ttf":[["Retroica.438d6e29.ttf","MyoF"],"MyoF"],"./../images/number_bg.png":[["number_bg.44c45aeb.png","Z7Zz"],"Z7Zz"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.2ffd701e.js.map
+//# sourceMappingURL=/HelpUkraine/src.7d606b04.js.map
